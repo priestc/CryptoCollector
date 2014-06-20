69dd1393c33cbb45ec2424a21735502760a63968
Chris Priest <nbvfour@gmail.com>
2014-06-19 20:14:28 -0700

    fixed urls

--- CoinStove/urls.py
+++ CoinStove/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'CoinStove.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'main.views.home', name='home'),
+    url(r'^wallet/', include('wallet.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
