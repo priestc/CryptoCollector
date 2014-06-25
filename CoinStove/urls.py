f00ab40ea3844fff3ae52dd961c3cf195f744be5
Chris Priest <nbvfour@gmail.com>
2014-06-24 23:08:52 -0700

    simple authentication

--- CoinStove/urls.py
+++ CoinStove/urls.py
@@ -5,9 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'main.views.home', name='home'),
+    url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'home.html'}),
     url(r'^wallets/', include('wallet.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^logout$', 'django.contrib.auth.views.logout', name='logout')
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 )
